d1f9b2105f880b44a377c6c11579d3644bf1c1b3
zodakzach <zachary.cervenka@gmail.com>
2025-07-30 20:50:13 -0400

    fix: convex client provider name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "@/app/ConvexBetterAuthProvider";
+import { ConvexClientProvider } from "@/app/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
